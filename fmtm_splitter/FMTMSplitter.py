20505a7b12ace7f130d8878e2282153c6956a26d
ivangayton <ivangayton@gmail.com>
2023-08-11 23:20:24 +0900

    Give up working in Rob's class structure, single-purpose function

--- fmtm_splitter/FMTMSplitter.py
+++ fmtm_splitter/FMTMSplitter.py
@@ -7,7 +7,7 @@
 #     the Free Software Foundation, either version 3 of the License, or
 #     (at your option) any later version.
 #
-#     OSM-Fieldwork is distributed in the hope that it will be useful,
+#     FMTM-Splitter is distributed in the hope that it will be useful,
 #     but WITHOUT ANY WARRANTY; without even the implied warranty of
 #     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #     GNU General Public License for more details.
@@ -26,93 +26,64 @@ from osgeo import gdal
 
 # Instantiate logger
 log = logging.getLogger(__name__)
-# Splitting algorithm choices
-choices = ('squares', 'file', 'custom', 'buildings')
 
-class FMTMSplitter(object):
-    """A class to split polygons"""
-    def __init__(self,
-                 aoi: str, # GeoJSON polygon filepath
-                 algorithm: str=None,
-                 ):
-        self.aoi = aoi
-        self.algorithm = algorithm
-        if algorithm == 'buildings':
-            self.splitByBuildings
-
-    def splitByBuildings(self,
-                         aoi: str, # GeoJSON polygon input file
-                         queries: list, # list of SQL queries
-                         dbd: list, # database host, dbname, user, password
-                         ):
-        """Split the polygon by buildings in the database using an SQL query"""
-        dbstring = (f"PG:host={dbd[0]} dbname={dbd[1]} "
-                    f"user={dbd[2]} password={dbd[3]}")
-        dbshell = psycopg2.connect(host=dbd[0], database = dbd[1],
-                                   user = dbd[2], password = dbd[3])  
-        dbshell.autocommit = True
-        dbcursor = dbshell.cursor()
-        dbcursor.execute('DROP TABLE IF EXISTS aoi;')
-        # Add the AOI to the database
-        log.info(f'Writing {aoi} to database as aoi layer.')
-        gdal.VectorTranslate(dbstring, aoi, layerName='aoi')
-        dbcursor.execute('DROP TABLE IF EXISTS project_aoi;'
-                         'CREATE TABLE project_aoi AS (SELECT '
-                         'ogc_fid as fid,wkb_geometry AS geom FROM aoi);'
-                         'ALTER TABLE project_aoi ADD PRIMARY KEY(fid);'
-                         'CREATE INDEX project_aoi_idx '
-                         'ON project_aoi USING GIST (geom);'
-                         'DROP TABLE aoi;')
+def splitByBuildings(aoi: str, # GeoJSON polygon input file
+                     queries: list, # list of SQL queries
+                     dbd: list, # database host, dbname, user, password
+                     ):
+    """Split the polygon by buildings in the database using an SQL query"""
+    dbstring = (f"PG:host={dbd[0]} dbname={dbd[1]} "
+                f"user={dbd[2]} password={dbd[3]}")
+    dbshell = psycopg2.connect(host=dbd[0], database = dbd[1],
+                               user = dbd[2], password = dbd[3])  
+    dbshell.autocommit = True
+    dbcursor = dbshell.cursor()
+    dbcursor.execute('DROP TABLE IF EXISTS aoi;')
+    # Add the AOI to the database
+    log.info(f'Writing {aoi} to database as aoi layer.')
+    gdal.VectorTranslate(dbstring, aoi, layerName='aoi')
+    dbcursor.execute('DROP TABLE IF EXISTS project_aoi;'
+                     'CREATE TABLE project_aoi AS (SELECT '
+                     'ogc_fid as fid,wkb_geometry AS geom FROM aoi);'
+                     'ALTER TABLE project_aoi ADD PRIMARY KEY(fid);'
+                     'CREATE INDEX project_aoi_idx '
+                     'ON project_aoi USING GIST (geom);'
+                     'DROP TABLE aoi;')
+    dbcursor.execute('VACUUM ANALYZE')
+    for query in queries:
+        dbcursor.execute(query)
         dbcursor.execute('VACUUM ANALYZE')
-        for query in queries:
-            dbcursor.execute(query)
-            dbcursor.execute('VACUUM ANALYZE')
-        log.info('Might very well have completed successfully')
+    log.info('Might very well have completed successfully')
 
 if __name__ == "__main__":
     # Command Line options
-    parser = argparse.ArgumentParser(
+    p = argparse.ArgumentParser(
         prog="FMTMSplitter.py",
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description="This program splits a Polygon AOI into tasks",
         epilog="""
-    This program splits a Polygon (the Area Of Interest)
-
-    examples:
-        fmtm-splitter -b AOI 
-        fmtm-splitter -v -b AOI -s data.geojson
-        fmtm-splitter -v -b AOI -s PG:colorado
-        
-        Where AOI is the boundary of the project as a polygon
-        And OUTFILE is a MultiPolygon output file, defaults to fmtm.geojson
+        This program splits a Polygon (the Area Of Interest)
+        examples:
         """
     )
-    parser.add_argument("-b", "--boundary", required=True,
+    p.add_argument("-b", "--boundary", required=True,
                         help="Polygon AOI GeoJSON file")
-    parser.add_argument("-n", "--numfeatures", default=20,
+    p.add_argument("-n", "--numfeatures", default=20,
                         help="Number of features on average desired per task")
-    parser.add_argument("-v", "--verbose",
+    p.add_argument("-v", "--verbose",
                         action="store_true", help="verbose output")
-    parser.add_argument("-o", "--outfile",
+    p.add_argument("-o", "--outfile",
                         default='fmtm.geojson',
                         help="Output file from splitting")
-    parser.add_argument("-a", "--algorithm", default='buildings',
-                        choices=choices, help="Splitting Algorithm to use")
-    parser.add_argument("-m", "--meters",
-                        help="Size in meters if using square splitting")
-    parser.add_argument("-s", "--source",
-                        help="Source data, Geojson or PG:[dbname]")
-    parser.add_argument("-c", "--custom",
-                        help="Custom SQL query for database]")
-    parser.add_argument("-ho", "--host", help="Database host",
+    p.add_argument("-ho", "--host", help="Database host",
                         default='localhost')
-    parser.add_argument("-db", "--database", help="Database to use")
-    parser.add_argument("-u", "--user", help="Database username")
-    parser.add_argument("-p", "--password", help="Database password")
+    p.add_argument("-db", "--database", help="Database to use")
+    p.add_argument("-u", "--user", help="Database username")
+    p.add_argument("-p", "--password", help="Database password")
 
-    args = parser.parse_args()
+    args = p.parse_args()
     if len(argv) < 2:
-        parser.print_help()
+        p.print_help()
         quit()
 
     # if verbose, dump to the terminal.
@@ -133,32 +104,24 @@ if __name__ == "__main__":
     # log.info("info")
     # log.info("warning")
 
-    # Read in the project AOI, which needs to be a GeoJson file
+    # Read in the project AOI, a GeoJSON file containing a polygon
     aoi = args.boundary
-    splitter = FMTMSplitter(aoi)
-
-    if args.algorithm == 'buildings':
-        log.debug(f"Splitting the AOI by SQL query")
-        modulardir = os.path.join(os.path.dirname(__file__),
-                                  'fmtm-splitter_osm_buildings')
-        modularsqlfiles = [
-            "fmtm-split_01_split_AOI_by_existing_line_features.sql",
-            "fmtm-split_02_count_buildings_for_subsplitting.sql",
-            "fmtm-split_03_cluster_buildings.sql",
-            "fmtm-split_04_create_polygons_around_clustered_buildings.sql",
-            "fmtm-split_05_clean_temp_files.sql"
-        ]
-        modularqueries = []
-        for sqlfile in modularsqlfiles:
-            with open(os.path.join(modulardir, sqlfile), 'r') as sql:
-                modularqueries.append(sql.read()
-                                      .replace('{%numfeatures%}',
-                                               str(args.numfeatures)))
-        dbdetails = [args.host, args.database, args.user, args.password]
-        features = splitter.splitByBuildings(aoi,
-                                             modularqueries,
-                                             dbdetails)
-        #log.info(f"Wrote splitBySQL.geojson")
-
-
-
+    modulardir = os.path.join(os.path.dirname(__file__),
+                              'fmtm-splitter_osm_buildings')
+    modularsqlfiles = [
+        "fmtm-split_01_split_AOI_by_existing_line_features.sql",
+        "fmtm-split_02_count_buildings_for_subsplitting.sql",
+        "fmtm-split_03_cluster_buildings.sql",
+        "fmtm-split_04_create_polygons_around_clustered_buildings.sql",
+        "fmtm-split_05_clean_temp_files.sql"
+    ]
+    modularqueries = []
+    for sqlfile in modularsqlfiles:
+        with open(os.path.join(modulardir, sqlfile), 'r') as sql:
+            modularqueries.append(sql.read()
+                                  .replace('{%numfeatures%}',
+                                           str(args.numfeatures)))
+    dbdetails = [args.host, args.database, args.user, args.password]
+    features = splitByBuildings(aoi,
+                                modularqueries,
+                                dbdetails)
