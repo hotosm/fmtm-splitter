4283cad29e4edf7ed5363130373ea90f419bb127
sam <sam.woodcock@protonmail.com>
2023-08-24 16:20:15 +0100

    build: add docker config for ci/prod

---
+++ build_imgs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eo pipefail
+
+PKG_VERSION=0.1.0
+
+docker build . --push \
+    -t "ghcr.io/hotosm/fmtm-splitter:${PKG_VERSION}" \
+    --target prod \
+    --build-arg PKG_VERSION="${PKG_VERSION}"
+
+docker build . --push \
+    -t ghcr.io/hotosm/fmtm-splitter:ci \
+    --target ci \
+    --build-arg PKG_VERSION="${PKG_VERSION}"
