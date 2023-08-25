574e771555ab586309f869ee00b2c52b79e4b5c8
sam <sam.woodcock@protonmail.com>
2023-08-25 16:50:56 +0100

    build: separate build / push stages ci img

--- build_imgs.sh
+++ build_imgs.sh
@@ -4,12 +4,16 @@ set -eo pipefail
 
 PKG_VERSION=0.1.0
 
-docker build . --push \
+docker build . \
     -t "ghcr.io/hotosm/fmtm-splitter:${PKG_VERSION}" \
     --target prod \
     --build-arg PKG_VERSION="${PKG_VERSION}"
+
+docker push "ghcr.io/hotosm/fmtm-splitter:${PKG_VERSION}"
 
 docker build . --push \
     -t ghcr.io/hotosm/fmtm-splitter:ci \
     --target ci \
     --build-arg PKG_VERSION="${PKG_VERSION}"
+
+docker push ghcr.io/hotosm/fmtm-splitter:ci
